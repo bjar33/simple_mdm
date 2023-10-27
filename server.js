625f087be130fca78c467a1c2b4222a50e9201e7
bjar33 <147496671+bjar33@users.noreply.github.com>
2023-10-27 11:28:17 +0200

    authorization

--- server.js
+++ server.js
@@ -1,62 +1,82 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const sqlite3 = require('sqlite3').verbose();
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcryptjs');
 
 const app = express();
-const db = new sqlite3.Database(':memory:'); // This creates an in-memory SQLite database.
+const db = new sqlite3.Database(':memory:');
 
-// Middleware
 app.use(bodyParser.json());
-app.use(express.static(__dirname)); // Serve static files from the root directory
+app.use(express.static(__dirname));
 
-// Database Initialization
+// **New Code: Start Session Management**
+app.use(express.session({ secret: 'some-secret-key', resave: false, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+// **End of New Code**
+
 db.serialize(() => {
     db.run("CREATE TABLE devices (id TEXT PRIMARY KEY, name TEXT, model TEXT)");
+    // **New Code: User Model**
+    db.run("CREATE TABLE users (id INTEGER PRIMARY KEY, username TEXT, password TEXT)");
+    // **End of New Code**
 });
-// Endpoint to register a device
-app.post('/register', (req, res) => {
-    const { id, name, model } = req.body;
-    if (!id || !name || !model) {
-        return res.status(400).send('Missing device information');
+
+// **New Code: Passport Strategies and User Routes**
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+        db.get("SELECT * FROM users WHERE username = ?", username, (err, user) => {
+            if (err) { return done(err); }
+            if (!user) { return done(null, false); }
+            if (!bcrypt.compareSync(password, user.password)) { return done(null, false); }
+            return done(null, user);
+        });
     }
+));
 
-    const stmt = db.prepare("INSERT INTO devices (id, name, model) VALUES (?, ?, ?)");
-    stmt.run(id, name, model, (err) => {
-        if (err) {
-            return res.status(500).send('Failed to register device');
-        }
-        res.send('Device registered successfully');
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+    db.get("SELECT * FROM users WHERE id = ?", id, (err, user) => {
+        done(err, user);
     });
 });
-if (!id || !name || !model) {
-    alert("All fields are mandatory and must meet the specified length criteria.");
-    return;
-}
 
-// Endpoint to list all registered devices
-app.get('/devices', (req, res) => {
-    db.all("SELECT * FROM devices", [], (err, rows) => {
-        if (err) {
-            return res.status(500).send('Failed to fetch devices');
-        }
-        res.json(rows);
+app.post('/register-user', (req, res) => {
+    const { username, password } = req.body;
+    const hashedPassword = bcrypt.hashSync(password, 8);
+    const stmt = db.prepare("INSERT INTO users (username, password) VALUES (?, ?)");
+    stmt.run(username, hashedPassword, (err) => {
+        if (err) return res.status(500).send("There was a problem registering the user.");
+        res.status(200).send("User registered successfully!");
     });
 });
+
+app.post('/login', passport.authenticate('local', { failureRedirect: '/' }), (req, res) => {
+    res.redirect('/');
+});
+
+app.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
+});
+// **End of New Code**
+
+app.get('/', (req, res) => {
+    res.send('MDM Server is running');
+});
+
+// ... Your other routes ...
 
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
-app.post('/register', (req, res) => {
-    const { id, name, model } = req.body;
 
-    // Validate data
-    if (!id || !name || !model || name.length < 3 || name.length > 50 || model.length < 2 || model.length > 30) {
-        return res.status(400).send('Invalid device information');
-    }
-
-    // ... rest of your route logic
-});
 
 
 
